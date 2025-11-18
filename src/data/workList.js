f48f5af3622e023ca584ddddc3c0fba951d53a10
KienVo2407 <vtk2407@gmail.com>
2025-11-18 10:51:12 -0800

    fixed worklist.js

--- src/data/workList.js
+++ src/data/workList.js
@@ -15,29 +15,17 @@ const workList = [
   },
   {
     id: 3,
-<<<<<<< Updated upstream
-    title: "Landmark Website",
-    category: "UI/UX",
-    image: "/project/Landmark/banner.png",
-    route: "/Landmark",
-=======
     title: "Kajaa Advertisement",
     category: "Video directing",
     image: "/project/KLVE/Banner.jpg",
     route: "/Kajaa",
->>>>>>> Stashed changes
   },
  {
     id: 4,
     title: "Reverb",
     category: "UI/UX",
-<<<<<<< Updated upstream
-    image: "/project/Lankmark/banner.png",
-    route: "/Reverb"
-=======
     image: "/project/Landmark/Landmark.png",
     route: "/Landmark"
->>>>>>> Stashed changes
   },
 ];
 export default workList;
