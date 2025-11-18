d343997010c6df0ab1a040622da0a453e8974a15
KienVo2407 <vtk2407@gmail.com>
2025-11-18 10:47:29 -0800

    fixed syntax error

--- src/pages/Project2/Project2.jsx
+++ src/pages/Project2/Project2.jsx
@@ -75,11 +75,7 @@ const Project2 = () => {
                  <section className="branding-showcase">
                     <div className="branding-header">
                         <AnimatedCopy tag="h3" animateOnScroll={true}>
-<<<<<<< Updated upstream
                            New Brand
-=======
-                            Brand Evolution
->>>>>>> Stashed changes
                         </AnimatedCopy>
                     </div>
 
