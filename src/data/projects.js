1caf28940f60d1f402aeb8863fdd5c30980a946b
KienVo2407 <vtk2407@gmail.com>
2025-11-18 10:54:13 -0800

    fixed projects.js

--- src/data/projects.js
+++ src/data/projects.js
@@ -9,30 +9,12 @@ const projects = [
   {
     id: 2,
     title: "KLVE",
-<<<<<<< Updated upstream
-    description: "The look that you happy with",
-    image: "/project/KLVE/Banner.jpg",
-=======
     description: "Branding + Marketing",
     image: "/project/KLVE/Producttags.jpg",
->>>>>>> Stashed changes
     route: "/KLVEstudio",
   },
   {
     id: 3,
-<<<<<<< Updated upstream
-    title: "Landmark Redesign",
-    description: "Better look for stand out features",
-    image: "/project/Landmark/banner.png",
-    route: "/Landmark",
-  },
-  {
-    id: 4,
-    title: "Reverb",
-    description: "Where the music touch feeling",
-    image: "/project/RejuvMD/Banner.jpg",
-    route: "/Reverb"
-=======
     title: "Kajaa Advertisement",
     description: "Video directing",
     image: "/project/KLVE/Banner.jpg",
@@ -42,7 +24,6 @@ const projects = [
     title: "Landmark Website",
     description: "UI/UX",
     image: "/project/Landmark/Landmark.png",
->>>>>>> Stashed changes
   },
 ];
 
