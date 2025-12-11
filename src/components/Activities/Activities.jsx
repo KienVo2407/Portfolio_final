97099f116a168dde088bc62ffa6e433db6f5daac
KienVo2407 <vtk2407@gmail.com>
2025-12-11 14:32:19 -0800

    fixing image issue

--- src/components/Activities/Activities.jsx
+++ src/components/Activities/Activities.jsx
@@ -53,7 +53,7 @@ const Activities = () => {
     },
     {
       id: 7,
-      image: "./public/about/gym.PNG",
+      image: "/about/gym.PNG",
       alt: "Badminton court scene - sports activity",
       type: "gym"
     },
